8b6ca104865ca9843215bb91c878fcb9a0131a2b
matiasurrutiaa <matiasurrutiaolaechea@gmail.com>
2022-12-13 01:32:26 -0300

    conexion

--- server.go
+++ server.go
@@ -3,30 +3,11 @@ package main
 import "github.com/gin-gonic/gin"
 
 func main() {
-	router := gin.Default()
+	r := gin.Default()
 
-	router.GET("/:param?query=Matias", func(c *gin.Context) {
-		param := c.Param("param")
-		query := c.Query("param")
-
-		cadena := "Parametro: " + param + "Query: " + query
-
-		c.JSON(200, cadena)
+	r.GET("/", func(c *gin.Context) {
+		c.String(200, "Here is first srver")
 	})
-
-	router.POST("/", func(c *gin.Context) {
-		c.JSON(200, "POST")
-
-	})
-
-	router.PUT("/", func(c *gin.Context) {
-		c.JSON(200, "PUT")
-	})
-
-	router.DELETE("/", func(c *gin.Context) {
-		c.JSON(200, "DELETE")
-	})
-
-	router.Run(":4001")
+	r.Run()
 
 }
